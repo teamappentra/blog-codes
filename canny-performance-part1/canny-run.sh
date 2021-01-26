adba2001377de99016c38896f36901399a77e189
Ivica Bogosavljevic <ivica@johnysswlab.com>
2021-01-26 10:07:53 +0100

    Canny-performance-part1: Add compilation and run script

---
+++ canny-performance-part1/canny-run.sh
@@ -0,0 +1,18 @@
+wget --no-clobber https://www.appentra.com/wp-content/uploads/2021/01/15360_8640.zip 
+
+unzip -o 15360_8640.zip
+
+
+CC=clang
+CFLAGS="-O3 -ffast-math -g -lm"
+
+${CC} ${CFLAGS} canny-optimized.c -o canny-optimized
+${CC} ${CFLAGS} canny-original.c -o canny-original
+
+CANNYPARAMS="testvecs/input/15360_8640.pgm 0.5 0.7 0.9"
+TIMECMD="/usr/bin/time -f'%E'"
+echo "Running canny-original"
+${TIMECMD} ./canny-original ${CANNYPARAMS}
+
+echo "Running canny-optimized"
+${TIMECMD} ./canny-optimized ${CANNYPARAMS}
