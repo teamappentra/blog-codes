ba0e62f125f73fa08630daa057b8fe7c669698e1
Ivica Bogosavljevic <ibogosavljevic@gmail.com>
2021-04-06 15:57:02 +0200

    Update precision-math-errno/compile.sh
    Co-authored-by: javinovo <javier.novo@appentra.com>

--- precision-math-errno/compile.sh
+++ precision-math-errno/compile.sh
@@ -1,6 +1,5 @@
 set -o xtrace
 
-ARGS="-g -O3 -march=core-avx2 -Rpass-missed=loop-vectorize -Rpass=loop-vectorize -Rpass-analysis=loop-vectorize"
 ARGS="-g -O3 -march=core-avx2"
 CC=gcc
 ${CC} ${ARGS} precision-math-errno.c -o precision-math-errno  -lm
