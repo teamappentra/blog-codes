726c672a9666543d368cbfe8e1645a1337498066
Ivica Bogosavljevic <ivica@johnysswlab.com>
2021-04-14 14:50:18 +0200

    Reverth previous change

--- precision-omp-simd/compile.sh
+++ precision-omp-simd/compile.sh
@@ -1,6 +1,6 @@
 set -o xtrace
 
-ARGS="-g -O3 -march=core-avx2 -fno-math-errno"
+ARGS="-g -O3 -march=core-avx2 -fno-math-errno -Rpass-missed=loop-vectorize -Rpass=loop-vectorize -Rpass-analysis=loop-vectorize"
 CC=clang
 ${CC} ${ARGS} precision-omp-simd.c -o precision-noomp-simd  -lm
 ${CC} ${ARGS} -fopenmp precision-omp-simd.c -o precision-omp-simd -lm
